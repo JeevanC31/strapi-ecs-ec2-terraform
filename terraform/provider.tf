0fdcae3711d60de67b0033d2699b69b5de5244ad
Jeevan C <162460307+JeevanC31@users.noreply.github.com>
2026-02-17 18:04:49 +0530

    Fix ECR dynamic URI

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,3 +1,12 @@
 provider "aws" {
   region = "us-east-1"
 }
+terraform {
+  backend "s3" {
+    bucket         = "your-terraform-state-bucket"
+    key            = "task7/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-lock"
+    encrypt        = true
+  }
+}
