829e97d53da16a78bfa4f249a92935f6381b22e1
Jeevan C <162460307+JeevanC31@users.noreply.github.com>
2026-02-17 18:11:21 +0530

    Fix

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -3,10 +3,10 @@ provider "aws" {
 }
 terraform {
   backend "s3" {
-    bucket         = "your-terraform-state-bucket"
-    key            = "task7/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "terraform-lock"
-    encrypt        = true
+    bucket  = "task7-terraform-state-jeevan-811738710312"
+    key     = "task7/terraform.tfstate"
+    region  = "us-east-1"
+    encrypt = true
   }
 }
+
